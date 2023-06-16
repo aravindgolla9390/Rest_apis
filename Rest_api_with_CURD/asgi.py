9af1c2a16488524de8b55f60e755f0e800ab3ca8
aravindgoll822 <aravindgoll822@gmail.com>
2023-06-16 17:21:52 +0530

    last modifications

--- Rest_api_with_CURD/asgi.py
+++ Rest_api_with_CURD/asgi.py
@@ -14,3 +14,5 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Rest_api_with_CURD.settings')
 
 application = get_asgi_application()
+
+# "thi sis the addintina; feauture"
